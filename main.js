83fac8035bf8b9a36b021f087127330fb62aa85e
webapis <webapis.github@gmail.com>
2021-10-12 09:03:09 +0500

    v

--- main.js
+++ main.js
@@ -5,15 +5,42 @@ const { puppeteerCrawler } = require('wflows')
 const { handlePageFunction } = require('./handlePageFunction')
 
 
-module.exports=function (){
+module.exports = function () {
 
     console.log('main js books workflow')
+    process.on('exit', async () => {
 
-    puppeteerCrawler({ handlePageFunction, headless: true, preNavHook: null, postNavHook: null,
-    
+        console.log('upload artifacts')
+        if (process.env.LOCAL !== 'TRUE') {
+            console.log('upload artifacts inside')
+            try {
+                console.log('outputPath', outputPath)
+                const artifactClient = artifact.create()
+                const artifactName = 'data-artifact';
+                const files = [
+                    outputPath
+                ]
+                const rootDirectory = process.cwd()
+                console.log('rootDirectory', rootDirectory)
+                const options = {
+                    continueOnError: false
+                }
+
+                const uploadResult = await artifactClient.uploadArtifact(artifactName, files, rootDirectory, options)
+                console.log('uploadResult', uploadResult)
+            } catch (error) {
+                console.log('error uploading artifacts', error)
+            }
+
+        }
+    })
+    puppeteerCrawler({
+        handlePageFunction, headless: true, preNavHook: null, postNavHook: null,
+
         urls: [{ url: 'https://books.toscrape.com/catalogue/category/books/religion_12/index.html', userData: {}, batchName: 'books', unshift: false, retry: false, retries: 0, sync: false }],
-     
-        batches: [{ batchName: 'books', concurrencyLimit: 20, retries: 3 }] })
+
+        batches: [{ batchName: 'books', concurrencyLimit: 20, retries: 3 }]
+    })
 }
 
 
