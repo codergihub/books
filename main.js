4a2227592b7a21e8f9a88c05c333783aba00c434
webapis <webapis.github@gmail.com>
2021-10-12 16:18:33 +0500

    v

--- main.js
+++ main.js
@@ -3,14 +3,14 @@
 const { puppeteerCrawler } = require('wflows')
 
 const { handlePageFunction } = require('./handlePageFunction')
-
+const os = require('os');
 module.exports = async function () {
 
     console.log('main js books workflow')
 
-   // process.env.ACTIONS_RUNTIME_URL=`${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`
+    // process.env.ACTIONS_RUNTIME_URL=`${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`
 
- const crawler=await   puppeteerCrawler({
+    const crawler = await puppeteerCrawler({
         handlePageFunction, headless: true, preNavHook: null, postNavHook: null,
 
         urls: [{ url: 'https://books.toscrape.com/catalogue/category/books/religion_12/index.html', userData: {}, batchName: 'books', unshift: false, retry: false, retries: 0, sync: false }],
@@ -21,7 +21,7 @@ module.exports = async function () {
     crawler.on('BROWSER_CLOSED', async () => {
         console.log('exiting....')
         console.log('upload artifacts')
- 
+        console.log('hostname', os.hostname())
 
     })
 }
