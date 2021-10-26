277bdfb95898dfe0b769534e71698bfc59f7f519
webapis <webapis.github@gmail.com>
2021-10-26 13:35:17 +0500

    v

--- main.js
+++ main.js
@@ -3,12 +3,10 @@
 const { puppeteerCrawler } = require('wflows')
 
 const { handlePageFunction } = require('./handlePageFunction')
-const os = require('os');
-module.exports = async function () {
+
+ async function crawler () {
 
     console.log('main js books workflow')
-
-    // process.env.ACTIONS_RUNTIME_URL=`${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}`
 
     const crawler = await puppeteerCrawler({
         handlePageFunction, headless: true, preNavHook: null, postNavHook: null,
@@ -21,10 +19,9 @@ module.exports = async function () {
     crawler.on('BROWSER_CLOSED', async () => {
         console.log('exiting....')
         console.log('upload artifacts')
-        console.log('hostname', os.hostname())
-        console.log('os.networkInterfaces()',os.networkInterfaces())
+
 
     })
 }
 
-
+crawler()
