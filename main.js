c62fbbfc585a9e151a4e6732d6022529edf14bd4
webapis <webapis.github@gmail.com>
2021-10-30 08:32:47 +0500

    v

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ const { handlePageFunction } = require('./handlePageFunction')
 async function crawler() {
 
     console.log('main js books workflow',process.env.PAGE_URL,process.env)
-
+debugger;
     const crawler = await puppeteerCrawler({
         handlePageFunction, headless: true, preNavHook: null, postNavHook: null,
-    
+        
         urls: [{ url: process.env.PAGE_URL, userData: {}, batchName: 'books', unshift: false, retry: false, retries: 0, sync: false }],
 
         batches: [{ batchName: 'books', concurrencyLimit: 20, retries: 3 }]
